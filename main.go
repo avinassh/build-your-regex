e517b7090aff1deeb62ae323de5fc68e8d1716a8
avi <hi@avi.im>
2022-02-28 19:13:50 +0530

    add code and tests

--- main.go
+++ main.go
@@ -2,10 +2,66 @@ package main
 
 import "fmt"
 
-func hi() string  {
-	return "hello world"
+func match(regexp, text string) bool {
+	if len(regexp) > 1 && regexp[0] == '^' {
+		return matchHere(regexp[1:], text)
+	}
+	if matchHere(regexp, text) {
+		return true
+	}
+	for {
+		if len(text) == 0 {
+			break
+		}
+		if matchHere(regexp, text) {
+			return true
+		}
+		text = text[1:]
+	}
+	return false
+}
+
+func matchHere(regexp, text string) bool {
+	if regexp == "" {
+		return true
+	}
+	if len(regexp) > 1 && (regexp[1] == '*') {
+		return matchStar(regexp[0], regexp[2:], text)
+	}
+	if regexp == "$" {
+		return text == ""
+	}
+	if text != "" && (regexp[0] == '.' || regexp[0] == text[0]) {
+		return matchHere(regexp[1:], text[1:])
+	}
+	return false
+}
+
+func matchStar(char uint8, regexp, text string) bool {
+	if matchHere(regexp, text) {
+		return true
+	}
+	for {
+		if len(text) == 0 {
+			break
+		}
+		if text[0] == char || char == '.' {
+			if matchHere(regexp, text) {
+				return true
+			}
+		} else {
+			return false
+		}
+		text = text[1:]
+	}
+	return false
 }
 
 func main()  {
-	fmt.Println(hi())
+	fmt.Println(match(".*", "a"))
+	fmt.Println(match("a*b", "aab"))
+	fmt.Println(match("a*c", "aab"))
+	fmt.Println(match("^match end$", "match end"))
+	fmt.Println(match("match end", "match end"))
+	fmt.Println(match("a*c$", "aac"))
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,108 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	hi()
+// tests regexes containing just the string literals
+func TestMatchLiteral(t *testing.T) {
+	tests := []struct{
+		regex string
+		text string
+		expected bool
+	}{
+		{"a", "a", true},
+		{"a", "ab", true},
+		{"aa", "aa", true},
+		{"goo", "google", true},
+		{"le", "google", true},
+		{"a", "b", false},
+		{"aa", "ab", false},
+		{"ba", "ab", false},
+	}
+	for _, tt := range tests {
+		if got := match(tt.regex, tt.text); got != tt.expected {
+			t.Fatalf("failed for regex %s in %s, expected=%t got=%t", tt.regex, tt.text, tt.expected, got)
+		}
+	}
+}
+
+// tests '.' regex
+func TestMatchSingleCharacter(t *testing.T) {
+	tests := []struct{
+		regex string
+		text string
+		expected bool
+	}{
+		{".", "a", true},
+		{"a.", "ab", true},
+		{"a.a", "aaa", true},
+		{"..ng", "king", true},
+		{".", ".", true},
+		{"a.", "b", false},
+	}
+	for _, tt := range tests {
+		if got := match(tt.regex, tt.text); got != tt.expected {
+			t.Fatalf("failed for regex %s in %s, expected=%t got=%t", tt.regex, tt.text, tt.expected, got)
+		}
+	}
+}
+
+// tests '^' regex
+func TestMatchBeginnings(t *testing.T) {
+	tests := []struct{
+		regex string
+		text string
+		expected bool
+	}{
+		{"^a", "a", true},
+		{"^ab", "ab", true},
+		{"^aa", "aaa", true},
+		{"^le", "google", false},
+		{"^abc", "acabc", false},
+	}
+	for _, tt := range tests {
+		if got := match(tt.regex, tt.text); got != tt.expected {
+			t.Fatalf("failed for regex %s in %s, expected=%t got=%t", tt.regex, tt.text, tt.expected, got)
+		}
+	}
+}
+
+// tests '$' regex
+func TestMatchEnds(t *testing.T) {
+	tests := []struct{
+		regex string
+		text string
+		expected bool
+	}{
+		{"a$", "a", true},
+		{"ab$", "ab", true},
+		{"aa$", "aaa", true},
+		{"le$", "google", true},
+		{"go$", "google", false},
+		{"abc$", "abcac", false},
+	}
+	for _, tt := range tests {
+		if got := match(tt.regex, tt.text); got != tt.expected {
+			t.Fatalf("failed for regex %s in %s, expected=%t got=%t", tt.regex, tt.text, tt.expected, got)
+		}
+	}
+}
+
+// tests '*' regex
+func TestMatchStar(t *testing.T) {
+	tests := []struct{
+		regex string
+		text string
+		expected bool
+	}{
+		{"a*", "aaaaaa", true},
+		{"ab*", "ab", true},
+		{"ab*", "a", true},
+		{"aa*", "aaaa", true},
+		{"go*gle", "google", true},
+		{"go*gle", "gooooooogle", true},
+	}
+	for _, tt := range tests {
+		if got := match(tt.regex, tt.text); got != tt.expected {
+			t.Fatalf("failed for regex %s in %s, expected=%t got=%t", tt.regex, tt.text, tt.expected, got)
+		}
+	}
 }
